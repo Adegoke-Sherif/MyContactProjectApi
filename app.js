e7fd70848185bfacd044ec163457e51a6945b609
Adegoke Sherif <shiffyadegoke@yahoo.com>
2024-04-07 12:11:38 +0100

    myContactProject

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDb = require("./config/dbConnection")
 const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes")
 const errorHandler = require("./middleware/errorhandler");
 dotenv.config();
 
+connectDb()
 const app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/contacts", contactRoutes)
+app.use("/api/users", userRoutes)
+
 app.use(errorHandler)
 
 
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,42 @@
+const { constant} = require("../constant")
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  res.json({ message: err.message, stackTrace: err.stack})
-}
+  switch (statusCode) {
+    case constant.VALIDATION_ERROR:
+      res.json({ 
+        title: "Validation Failed", 
+        message: err.message, 
+        stackTrace: err.stack
+     });
+    break;
+  case constant.NOT_FOUND:
+  res.json({ 
+    title: "Not Found", 
+    message: err.message, 
+    stackTrace: err.stack
+    });
+  case constant.UNAUTHORIZED:
+    res.json({ 
+      title: "unauthorized", 
+      message: err.message, 
+      stackTrace: err.stack
+      });
+  case constant.FORBIDDEN:
+      res.json({ 
+        title: "forbidden", 
+        message: err.message, 
+        stackTrace: err.stack
+        });
+  case constant.SERVER_ERROR:
+        res.json({ 
+          title: "server_error", 
+          message: err.message, 
+          stackTrace: err.stack
+          });
+    default:
+      console.log("No Error, All good!")
+      break;
+  }
+};
 
 module.exports = errorHandler;
